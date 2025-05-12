4d18829af9d02fb44eaf76a3b3ce33ebfd827869
Misha <manon4ik@gmail.com>
2025-05-12 19:09:23 +0300

    step1

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,19 +2,35 @@ import { createStore } from "@reduxjs/toolkit";
 import { devToolsEnhancer } from "@redux-devtools/extension";
 
 const initialState = {
-    contacts: [
-      { id: 0, name: "Rosie Simpson", phone: '123-456-78' },
-      { id: 1, name: "Hermione Kline", phone: '234-567-81' },
-      { id: 2, name: "Eden Clements", phone: '345-678-12' },
-      { id: 3, name: "Annie Copeland", phone: '456-123-87' },
-    ],
-    filter: '',
-  };
+  contacts: [
+    { id: 0, name: "Rosie Simpson", phone: '123-456-78' },
+    { id: 1, name: "Hermione Kline", phone: '234-567-81' },
+    { id: 2, name: "Eden Clements", phone: '345-678-12' },
+    { id: 3, name: "Annie Copeland", phone: '456-123-87' },
+  ],
+  filter: '',
+};
 
-  const rootReducer = (state = initialState, action) => {
-    return state;
-  };
+const rootReducer = (state = initialState, action) => {
 
-  const enhancer = devToolsEnhancer();
+  switch (action.type) {
 
-  export const store = createStore(rootReducer, enhancer);
+    case "contacts/addContact":
+
+      return {
+        ...state,
+        contacts: [
+          ...state.contacts,
+          action.platload,
+        ],
+      }
+
+    default:
+      return state;
+  }
+
+};
+
+const enhancer = devToolsEnhancer();
+
+export const store = createStore(rootReducer, enhancer);
